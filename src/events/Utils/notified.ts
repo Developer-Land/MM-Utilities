d8a4621ef6e8b55d41d1e9d34d479dea9e284898
EXPress-016 <74526510+EXPress-016@users.noreply.github.com>
2022-06-21 14:33:59 +0530

    Fixed: Youtube notification

--- src/events/Utils/notified.ts
+++ src/events/Utils/notified.ts
@@ -4,13 +4,12 @@ import { client, notifier } from '../../index';
 import { Event } from '../../structures/Event';
 
 export default new Event(notifier, 'notified', (data: Notification) => {
-  const published = String(data.published).split(' ');
-  const updated = String(data.updated).split(' ');
-  if (published[2] === updated[2]) {
+  const published = data.published
+  const updated = data.updated
+  if (published !== updated) return;
     (
       client.channels.cache.get('904616970190725150') as GuildTextBasedChannel
     ).send(
       `Hey <@&875239770584911903>, **MM GAMER** just posted a video! Go check it out! ${data.video.link}`
     );
-  }
 });
