3ddc4afcf46c9210360b0c9a81b346d57f8d0d97
dorponjeetbaruah101@gmail.com <dorponjeetbaruah101@gmail.com>
2022-09-13 18:04:08 +0530

    Fixed: reply/edit after the promise is resolved

--- src/Buttons/Verification/accept.ts
+++ src/Buttons/Verification/accept.ts
@@ -7,19 +7,27 @@ export default new Button({
     const userId = interaction.customId.replace(/\D/g, '') as Snowflake;
     
     (interaction.guild.members.cache.get(userId) as GuildMember)
-      .roles.add("854647752197931038", `${interaction.user.tag} - Passed verification.`);
-      
-    interaction.update({
-      components: [
-        new MessageActionRow()
-          .addComponents(
-            new MessageButton()
-              .setStyle("SUCCESS")
-              .setLabel("Accepted")
-              .setDisabled(true)
-              .setCustomId("==============================================================================")
-          )
-      ]
-    });
+      .roles.add("854647752197931038", `${interaction.user.tag} - Passed verification.`)
+        .then(() => {
+          interaction.update({
+            components: [
+              new MessageActionRow()
+               .addComponents(
+                  new MessageButton()
+                  .setStyle("SUCCESS")
+                  .setLabel("Accepted")
+                  .setDisabled(true)
+                  .setCustomId("==============================================================================")
+                )
+            ]
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          interaction.reply({
+            content: 'An error accured.',
+            ephemeral: true
+          });
+        });
   },
 });
--- src/Buttons/Verification/reject_confirmed.ts
+++ src/Buttons/Verification/reject_confirmed.ts
@@ -12,21 +12,32 @@ export default new Button({
     const userId = ((targetMessage.components[0].components[1] as MessageButton).customId.replace(/\D/g, '')) as Snowflake;
     
     (interaction.guild.members.cache.get(userId) as GuildMember)
-      .kick(`${interaction.user.tag} - Failed verification.`);
-    
-    new Webhook({ channelId: "1008754426678353970", messageId: targetMessage.id }, {
-      components: [
-        new MessageActionRow()
-          .addComponents(
-            new MessageButton()
-            .setStyle("DANGER")
-            .setLabel("Declined/Kicked")
-            .setDisabled(true)
-            .setCustomId("================================================================================")
-          )
-      ]
-    }).edit();
-    
-    interaction.deferUpdate();
+      .kick(`${interaction.user.tag} - Failed verification.`)
+        .then(() => {
+          interaction.update({
+            content: "<:MM_yesyesyes:909139323692154880>",
+            components: []
+          });
+          
+          new Webhook({ channelId: "1008754426678353970", messageId: targetMessage.id }, {
+            components: [
+              new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                    .setStyle("DANGER")
+                    .setLabel("Declined/Kicked")
+                    .setDisabled(true)
+                    .setCustomId("================================================================================")
+                )
+            ]
+          }).edit();
+        })
+        .catch(err => {
+          console.log(err);
+          interaction.reply({
+            content: 'An error accured.',
+            ephemeral: true
+          });
+        });
   }
 });
