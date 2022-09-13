d27f01ccbebcc336a758040b39884b02ecfa1615
dorponjeetbaruah101@gmail.com <dorponjeetbaruah101@gmail.com>
2022-09-13 13:33:18 +0530

    Added: edit webhook method. Changed: property name and its data modifier

--- src/Structures/Webhook.ts
+++ src/Structures/Webhook.ts
@@ -1,17 +1,28 @@
 import { client } from "..";
-import { WebhookMessageOptions, WebhookClient, WebhookClientDataURL, WebhookClientDataIdWithToken, GuildTextBasedChannel, ThreadChannel, Snowflake } from "discord.js";
+import { 
+  WebhookMessageOptions,
+  WebhookEditMessageOptions,
+  WebhookClient,
+  WebhookClientDataURL,
+  WebhookClientDataIdWithToken,
+  GuildTextBasedChannel,
+  ThreadChannel,
+  Snowflake
+} from "discord.js";
 
 interface IWebhookData {
   url?: string;
   token?: string;
   id?: Snowflake;
   channelId?: Snowflake;
+  messageId?: Snowflake;
+  messageUrl?: string;
 }
 
 export class Webhook {
-  private _data: IWebhookData;
-  private _send: WebhookMessageOptions;
   private _createdWebhook: string;
+  public webhookData: IWebhookData;
+  public messageOptions: WebhookMessageOptions;
   
   /**
    * @example
@@ -21,9 +32,9 @@ export class Webhook {
    *   content: "Hello, this webhook is sent by MM Utilities#4628!"
    * }).send();
    */
-  constructor(data: IWebhookData, sendOptions: WebhookMessageOptions) {
-    this._data = data;
-    this._send = sendOptions;
+  constructor(data: IWebhookData, options: WebhookMessageOptions | WebhookEditMessageOptions) {
+    this.webhookData = data;
+    this.messageOptions = options;
   }
   
   private async createWebhook(channelId: Snowflake) {
@@ -31,7 +42,7 @@ export class Webhook {
   
     const channel = client.channels.cache.get(channelId) as GuildTextBasedChannel;
   
-    if (!channel || channel instanceof ThreadChannel) return false;
+    if (!channel || channel instanceof ThreadChannel) return;
   
     const myWebhooks = (await channel.fetchWebhooks()).filter(
       webhook => webhook.owner.id === client.user!.id
@@ -41,27 +52,27 @@ export class Webhook {
       myWebhooks.size !== 0 ?
       myWebhooks.first().url :
       (await channel.createWebhook(client.user.username, {
-        avatar: client.user.displayAvatarURL()
+        avatar: client.user.displayAvatarURL({ size: 4096 })
       })).url
     );
   
     this._createdWebhook = webhookUrl;
   }
-  
+
   public async send() {
     let webhookClientData: WebhookClientDataURL | WebhookClientDataIdWithToken | null;
   
     switch (true) {
-      case "url" in this._data:
-        webhookClientData = { url: this._data.url }
+      case "url" in this.webhookData:
+        webhookClientData = { url: this.webhookData.url }
         break;
   
-      case "id" in this._data && "token" in this._data:
-        webhookClientData = { id: this._data.id, token: this._data.token }
+      case "id" in this.webhookData && "token" in this.webhookData:
+        webhookClientData = { id: this.webhookData.id, token: this.webhookData.token }
         break;
   
-      case "channelId" in this._data:
-        await this.createWebhook(this._data.channelId);
+      case "channelId" in this.webhookData:
+        await this.createWebhook(this.webhookData.channelId);
         
         webhookClientData = (
           this._createdWebhook ?
@@ -75,9 +86,53 @@ export class Webhook {
     }
   
     if (webhookClientData === null) throw new Error("No valid webhook or channel specified.");
+
+    const webhookClient = new WebhookClient(webhookClientData!);
+    
+    return webhookClient.send(this.messageOptions);
+  }
   
-    const webhookClient = new WebhookClient(webhookClientData!)
-  
-    await webhookClient.send(this._send);
+  public async edit() {
+    let message: Message;
+    let channel: GuildTextBasedChannel;
+    
+    switch (true) {
+      case "messageUrl" in this.webhookData:
+        let url = this.webhookData.messageUrl.split("/");
+
+        url = url.slice(url.length - 3);
+        
+        channel = client.channels.cache.get(url[1]) as GuildTextBasedChannel;
+        message = await channel.messages.fetch(url[2]);
+        break;
+      
+      case "channelId" in this.webhookData && "messageId" in this.webhookData:
+        channel = client.channels.cache.get(this.webhookData.channelId) as GuildTextBasedChannel;
+        message = await channel.messages.fetch(this.webhookData.messageId);
+        break;
+      
+      default:
+        channel = null;
+        message = null;
+    }
+    
+    if (channel === null || message === null)
+      throw new Error("No valid message URL or channel and message ID specified.")
+   
+    if (!channel || channel instanceof ThreadChannel) return;
+   
+    const foundWebhook = (await channel.fetchWebhooks()).filter(webhook => {
+      return webhook.id === message.webhookId && webhook.owner.id === client.user!.id;
+    });
+    
+    if (!foundWebhook.size)
+      throw new Error("Couldn't find webhook.");
+    
+    const webhookClient = new WebhookClient({ url: foundWebhook.first().url });
+    
+    ["username", "avatarURL"]
+      .forEach(key => delete this.messageOptions[key]);
+    
+    webhookClient.editMessage(message.id, this.messageOptions);
   }
 }
